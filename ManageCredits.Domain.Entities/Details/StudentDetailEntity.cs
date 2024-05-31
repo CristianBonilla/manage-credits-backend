f790796c291405eaf050c15a38bd965ff5b66c41
Cristian Bonilla <cristiancamilo10_95@outlook.com>
2024-05-31 00:27:07 -0500

    :recycle: Student property added in detail

--- ManageCredits.Domain.Entities/Details/StudentDetailEntity.cs
+++ ManageCredits.Domain.Entities/Details/StudentDetailEntity.cs
@@ -7,8 +7,10 @@ public class StudentDetailEntity : BaseEntity
 {
   public Guid StudentDetailId { get; set; }
   public Guid TeacherDetailId { get; set; }
+  public Guid StudentId { get; set; }
   public Guid ClassId { get; set; }
   public required ClassStatus Status { get; set; }
   public TeacherDetailEntity TeacherDetail { get; set; } = null!;
+  public StudentEntity Student { get; set; } = null!;
   public ClassEntity Class { get; set; } = null!;
 }
