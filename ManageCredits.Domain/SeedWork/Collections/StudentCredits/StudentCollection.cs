cfbac36cda607b67deaa103ded20f0d2f7e606b4
Cristian Bonilla <cristiancamilo10_95@outlook.com>
2024-05-30 22:45:53 -0500

    :recycle: Creation property added for each seed data collection

--- ManageCredits.Domain/SeedWork/Collections/StudentCredits/StudentCollection.cs
+++ ManageCredits.Domain/SeedWork/Collections/StudentCredits/StudentCollection.cs
@@ -23,23 +23,27 @@ class StudentCollection : SeedDataCollection<StudentEntity>
           StudentDetailId = new("{11ab0e13-3a0a-4fd7-9f80-3dc89b181efb}"),
           TeacherDetailId = GetTeacherDetailId(0),
           ClassId = GetSubjectClassId(0, 0),
-          Status = ClassStatus.Completed
+          Status = ClassStatus.Completed,
+          Created = new DateTimeOffset(2024, 3, 2, 1, 0, 0, TimeSpan.Zero)
         },
         new StudentDetailEntity
         {
           StudentDetailId = new("{2096ecba-29db-49d6-9646-a6c3e424953f}"),
           TeacherDetailId = GetTeacherDetailId(0),
           ClassId = GetSubjectClassId(0, 1),
-          Status = ClassStatus.Completed
+          Status = ClassStatus.Completed,
+          Created = new DateTimeOffset(2024, 3, 3, 2, 1, 0, TimeSpan.Zero)
         },
         new StudentDetailEntity
         {
           StudentDetailId = new("{eab71419-9084-4e72-9558-ce4d76f0fd30}"),
           TeacherDetailId = GetTeacherDetailId(0),
           ClassId = GetSubjectClassId(0, 2),
-          Status = ClassStatus.Completed
+          Status = ClassStatus.Completed,
+          Created = new DateTimeOffset(2024, 3, 4, 3, 2, 0, TimeSpan.Zero)
         }
-      ]
+      ],
+      Created = new DateTimeOffset(2024, 3, 1, 0, 0, 0, TimeSpan.Zero)
     },
     new StudentEntity
     {
@@ -54,37 +58,43 @@ class StudentCollection : SeedDataCollection<StudentEntity>
           StudentDetailId = new("{90812e38-67ad-4207-8017-e2b09231231e}"),
           TeacherDetailId = GetTeacherDetailId(1),
           ClassId = GetSubjectClassId(1, 0),
-          Status = ClassStatus.InProgress
+          Status = ClassStatus.InProgress,
+          Created = new DateTimeOffset(2024, 3, 2, 1, 0, 0, TimeSpan.Zero)
         },
         new StudentDetailEntity
         {
           StudentDetailId = new("{6647c961-57b7-4e04-ad8f-3213a6b85f89}"),
           TeacherDetailId = GetTeacherDetailId(1),
           ClassId = GetSubjectClassId(1, 1),
-          Status = ClassStatus.Completed
+          Status = ClassStatus.Completed,
+          Created = new DateTimeOffset(2024, 3, 3, 2, 1, 0, TimeSpan.Zero)
         },
         new StudentDetailEntity
         {
           StudentDetailId = new("{7f8a160f-b415-486d-bce2-af44c58af36c}"),
           TeacherDetailId = GetTeacherDetailId(1),
           ClassId = GetSubjectClassId(1, 2),
-          Status = ClassStatus.InProgress
+          Status = ClassStatus.InProgress,
+          Created = new DateTimeOffset(2024, 3, 4, 3, 2, 0, TimeSpan.Zero)
         },
         new StudentDetailEntity
         {
           StudentDetailId = new("{fa2a8cc5-c7ae-4577-a981-3d4ab4a2ef1b}"),
           TeacherDetailId = GetTeacherDetailId(1),
           ClassId = GetSubjectClassId(1, 3),
-          Status = ClassStatus.InProgress
+          Status = ClassStatus.InProgress,
+          Created = new DateTimeOffset(2024, 3, 5, 4, 3, 0, TimeSpan.Zero)
         },
         new StudentDetailEntity
         {
           StudentDetailId = new("{58696965-ccef-46b8-b791-74c4f8c28f25}"),
           TeacherDetailId = GetTeacherDetailId(1),
           ClassId = GetSubjectClassId(1, 4),
-          Status = ClassStatus.Pending
+          Status = ClassStatus.Pending,
+          Created = new DateTimeOffset(2024, 3, 6, 5, 4, 0, TimeSpan.Zero)
         }
-      ]
+      ],
+      Created = new DateTimeOffset(2024, 3, 2, 0, 0, 0, TimeSpan.Zero)
     }
   ];
 
--- ManageCredits.Domain/SeedWork/Collections/StudentCredits/TeacherCollection.cs
+++ ManageCredits.Domain/SeedWork/Collections/StudentCredits/TeacherCollection.cs
@@ -23,15 +23,18 @@ class TeacherCollection : SeedDataCollection<TeacherEntity>
         {
           TeacherDetailId = new("{f87b9e01-7066-4a18-bbe5-560a9c6ddec2}"),
           SubjectId = _subjects[0].SubjectId,
-          TotalCredits = TOTAL_CREDITS
+          TotalCredits = TOTAL_CREDITS,
+          Created = new DateTimeOffset(2024, 2, 2, 1, 0, 0, TimeSpan.Zero)
         },
         new TeacherDetailEntity
         {
           TeacherDetailId = new("{f79f1e3c-8974-4b38-8f9d-72e738efb046}"),
           SubjectId = _subjects[1].SubjectId,
-          TotalCredits = TOTAL_CREDITS
+          TotalCredits = TOTAL_CREDITS,
+          Created = new DateTimeOffset(2024, 2, 3, 2, 1, 0, TimeSpan.Zero)
         }
-      ]
+      ],
+      Created = new DateTimeOffset(2024, 2, 1, 0, 0, 0, TimeSpan.Zero)
     }
   ];
 
